6064920476f563ba4632c27157e8880a95a0adb1
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2022-10-18 23:56:19 +0100

    add a box to overide chakra background

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React, { useEffect } from "react";
 import Homepage from "./pages/Homepage";
 import AOS from "aos";
+import { Box } from "@chakra-ui/react";
 function App() {
   useEffect(() => {
     AOS.init();
   }, []);
   return (
     <>
-      <Homepage />
+      <Box w={"100%"} minH={"100vh"} bg={"#181819"} color={"white"}>
+        <Homepage />
+      </Box>
     </>
   );
 }
