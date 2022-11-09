f6d843d4b9882efbf5f82534ef6e900922359943
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2022-11-09 23:32:15 +0100

    ...

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,46 @@
+import { Box, color, Flex, Text } from "@chakra-ui/react";
+import React from "react";
+import { Link } from "react-router-dom";
+const Navbar = () => {
+  return (
+    <Box
+      w={"300px"}
+      bg={"#2c2b2c"}
+      my={"20px"}
+      p={"10px"}
+      borderRadius={"30px"}
+    >
+      <Flex justifyContent={"center"} gap={"20px"}>
+        <Link to={"/"} mt={"20px"}>
+          <Text
+            _hover={{
+              color: "teal",
+            }}
+          >
+            Homepage
+          </Text>
+        </Link>
+        <Link to={"/devtofunmi/project"} mt={"20px"}>
+          <Text
+            _hover={{
+              color: "teal",
+            }}
+          >
+            Project
+          </Text>
+        </Link>
+        <Link to={"/devtofunmi/about"} mt={"20px"}>
+          <Text
+            _hover={{
+              color: "teal",
+            }}
+          >
+            About
+          </Text>
+        </Link>
+      </Flex>
+    </Box>
+  );
+};
+
+export default Navbar;
