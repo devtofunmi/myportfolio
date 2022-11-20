b2823292360963a089b4953451fae059f3e74d01
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2022-11-20 14:21:35 +0100

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,7 @@ import React, { useEffect } from "react";
 import Homepage from "./pages/Homepage";
 import AOS from "aos";
 import { Box } from "@chakra-ui/react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Project from "./pages/Project";
-import About from "./pages/About";
+
 function App() {
   useEffect(() => {
     AOS.init();
@@ -12,13 +10,7 @@ function App() {
   return (
     <>
       <Box w={"100%"} minH={"100vh"} bg={"black"} color={"white"}>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Homepage />} />
-            <Route path="/devtofunmi/project" element={<Project />} />
-            <Route path="/devtofunmi/about" element={<About />} />
-          </Routes>
-        </Router>
+        <Homepage />
       </Box>
     </>
   );
